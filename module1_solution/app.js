b2687792565ac5322d9d8428528051ad71fa9719
rpr5x <rpr5x@virginia.edu>
2016-10-04 19:48:54 -0400

    Finished first assignment

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -7,7 +7,39 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope){
+  $scope.dishes = ""
+  $scope.message = ""
+  $scope.myMessageColor = ""
+  $scope.myBorderColor = ""
 
+  // function to display the appropriate message to the user
+  $scope.showMessage = function(dishes) {
+
+   // check if user entered no data
+   if(dishes.length == 0){
+     $scope.message = "Please enter data first";
+     $scope.myMessageColor = {"color":"red"};
+     $scope.myBorderColor = {"border-color":"red"}
+     return
+   }
+
+    // split the input by comma delimiter
+    var my_dishes = dishes.split(",")
+
+    //  Handle cases where number of foods is less than or equal to 3
+    // and where number of foods is greater than 3. Also, set the
+    // appropriate colors around the message and textbox.
+    if(my_dishes.length <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.myMessageColor = {"color":"green"};
+      $scope.myBorderColor = {"border-color":"green"};
+    }
+    else {
+      $scope.message = "Too much!"
+      $scope.myMessageColor = {"color":"green"};
+      $scope.myBorderColor = {"border-color":"green"};
+    }
+  };
 
 };
 
