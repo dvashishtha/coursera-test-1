4bed09d3c22dff0e46f4f5c338de4cca2fdd43de
rpr5x <rpr5x@virginia.edu>
2016-10-23 17:19:10 -0400

    Added My Info page

---
+++ module5_solution/src/public/my-info/my-info.controller.js
@@ -0,0 +1,15 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MyInfoController', MyInfoController);
+
+MyInfoController.$inject = ['UserService', 'ApiPath'];
+function MyInfoController(UserService, ApiPath) {
+  var $myInfoCtrl = this;
+  $myInfoCtrl.userInfo = UserService.getUserPreferences();
+  $myInfoCtrl.basePath = ApiPath;
+  console.log($myInfoCtrl.userInfo);
+}
+
+})();
